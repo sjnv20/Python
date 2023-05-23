1e8a3d510a30637553092c191aa75b1303e5a8af
sulguk <sj-nv20@naver.com>
2023-05-23 16:44:42 +0900

    BaekJoon - 반복문

--- BaekJoon/15552_10951.py
+++ BaekJoon/15552_10951.py
@@ -1,6 +1,7 @@
 #Q_15552 : A + B
 # Python을 사용하고 있다면, input 대신 sys.stdin.readline을 사용할 수 있다.
 # 단, 이때는 맨 끝의 개행문자까지 같이 입력받기 때문에 문자열을 저장하고 싶을 경우 .rstrip()을 추가로 해 주는 것이 좋다.
+# 각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력
 import sys
 t = int(input())
 for i in range(t) :
